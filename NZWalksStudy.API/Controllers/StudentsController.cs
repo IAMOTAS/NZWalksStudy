538e54968f2c4e4d65a7e347d42a1ac9dc057e92
IAMOTAS <niiotasalokome@gmail.com>
2024-02-20 16:08:56 +0000

    initial commit

---
+++ NZWalksStudy.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksStudy.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetActionResult()
+        {
+            string[] students = new string[]
+            {
+                "Nii", "Okai","Tettey", "Antie","Samuel","Sidney"
+            };
+            return Ok(students);
+        }
+    }
+}
