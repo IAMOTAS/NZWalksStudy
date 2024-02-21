5bfdb396aa03f3ea65af8d88e2c9bc528e8cebcb
IAMOTAS <niiotasalokome@gmail.com>
2024-02-21 16:34:13 +0000

    Commiting Crud-properties of APIs

--- NZWalksStudy.API/Controllers/RegionsController.cs
+++ NZWalksStudy.API/Controllers/RegionsController.cs
@@ -78,11 +78,75 @@ namespace NZWalksStudy.API.Controllers
         [HttpPost]
         public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
         {
+            var region = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            }; 
+
+        zWalksDbContext.Regions.Add(region);
+            zWalksDbContext.SaveChanges();
+
+            var regionDto = new RegionDto
+            {
+                Id = region.Id,
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+            
+
+            return CreatedAtAction(nameof(GetById), new {id = region.Id},region);
 
         }
 
+        [HttpPut]
+        [Route("{id:Guid}")]
+            public IActionResult Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
+        {
+            var region = zWalksDbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+            region.Code = updateRegionRequestDto.Code;
+            region.Name = updateRegionRequestDto.Name;
+            region.RegionImageUrl = updateRegionRequestDto.RegionImageUrl;
+
+            zWalksDbContext.SaveChanges();
 
 
+            var Region = new Region
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+
+
+            return Ok(region);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            var region = zWalksDbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if ( region == null )
+            {
+                return NotFound();
+            }
+
+            zWalksDbContext.Regions.Remove(region);
+            zWalksDbContext.SaveChanges();
+            return Ok();
+
+        }
     }
 }
 
